4a917cf9688841ec33daf695c5b6e4d576eb6e5b
Ville <you@example.com>
2026-08-30 12:41:21 +0200

    add assignment 1

--- app/src/main.c
+++ app/src/main.c
@@ -5,27 +5,55 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_A 5
-#define PRIO_B 5
+#define PRIO_LOW 7
+#define PRIO_MED 5
+#define PRIO_HIGH 3
+#define PRIO_COOP -1
 
-void thread_a_fn(void *p1, void *p2, void *p3)
+void t_low_fn(void *p1, void *p2, void *p3)
 {
     while (1) {
-        k_msleep(200);
-    }
-}
-
-void thread_b_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
+        LOG_INF("T_LOW running");
         k_msleep(300);
     }
 }
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE, thread_a_fn,
-                NULL, NULL, NULL, PRIO_A, 0, 0);
-K_THREAD_DEFINE(thread_b, STACK_SIZE, thread_b_fn,
-                NULL, NULL, NULL, PRIO_B, 0, 0);
+void t_med_fn(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        LOG_INF("T_MED running");
+        k_msleep(200);
+    }
+}
+
+void t_high_fn(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        LOG_INF("T_HIGH running");
+        k_msleep(100);
+    }
+}
+
+void t_coop_fn(void *p1, void *p2, void *p3)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        LOG_INF("T_COOP running loop %i", i);
+        k_busy_wait(100000);
+    }
+    LOG_INF("T_COOP yielding, nothing interesting happens");
+    k_yield();
+    LOG_INF("T_COOP resumed, and exits letting others run");
+}
+
+K_THREAD_DEFINE(thread_a, STACK_SIZE, t_low_fn,
+                NULL, NULL, NULL, PRIO_LOW, 0, 0);
+K_THREAD_DEFINE(thread_b, STACK_SIZE, t_med_fn,
+                NULL, NULL, NULL, PRIO_MED, 0, 0);
+K_THREAD_DEFINE(thread_c, STACK_SIZE, t_high_fn,
+                NULL, NULL, NULL, PRIO_HIGH, 0, 0);
+K_THREAD_DEFINE(thread_d, STACK_SIZE, t_coop_fn,
+                NULL, NULL, NULL, PRIO_COOP, 0, 0);
 
 int main(void)
 {
