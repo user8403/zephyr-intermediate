7025532698d4e25a4c2a9d55d33d2a5f72504da9
Ville <you@example.com>
2026-08-30 14:25:52 +0200

    add assignment 2

--- app/src/main.c
+++ app/src/main.c
@@ -5,58 +5,32 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_LOW 7
-#define PRIO_MED 5
-#define PRIO_HIGH 3
-#define PRIO_COOP -1
+#define PRIO 1
 
-void t_low_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("T_LOW running");
-        k_msleep(300);
-    }
-}
+volatile uint32_t counter = 0;
+K_MUTEX_DEFINE(mymutex);
 
-void t_med_fn(void *p1, void *p2, void *p3)
+void t_work_fn(void *p1, void *p2, void *p3)
 {
-    while (1) {
-        LOG_INF("T_MED running");
-        k_msleep(200);
-    }
-}
-
-void t_high_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("T_HIGH running");
-        k_msleep(100);
-    }
-}
-
-void t_coop_fn(void *p1, void *p2, void *p3)
-{
-    for (int i = 0; i < 5; i++)
+    LOG_INF("running %u", (uint32_t)p1);
+    for (int i= 0; i < 1000000; i++)
     {
-        LOG_INF("T_COOP running loop %i", i);
-        k_busy_wait(100000);
+        k_mutex_lock(&mymutex, K_FOREVER);
+        counter++;
+        k_mutex_unlock(&mymutex);
     }
-    LOG_INF("T_COOP yielding, nothing interesting happens");
-    k_yield();
-    LOG_INF("T_COOP resumed, and exits letting others run");
 }
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE, t_low_fn,
-                NULL, NULL, NULL, PRIO_LOW, 0, 0);
-K_THREAD_DEFINE(thread_b, STACK_SIZE, t_med_fn,
-                NULL, NULL, NULL, PRIO_MED, 0, 0);
-K_THREAD_DEFINE(thread_c, STACK_SIZE, t_high_fn,
-                NULL, NULL, NULL, PRIO_HIGH, 0, 0);
-K_THREAD_DEFINE(thread_d, STACK_SIZE, t_coop_fn,
-                NULL, NULL, NULL, PRIO_COOP, 0, 0);
+K_THREAD_DEFINE(thread_a, STACK_SIZE, t_work_fn,
+                1, NULL, NULL, PRIO, 0, 0);
+K_THREAD_DEFINE(thread_b, STACK_SIZE, t_work_fn,
+                2, NULL, NULL, PRIO, 0, 0);
 
 int main(void)
 {
+    k_thread_join(thread_a, K_FOREVER);
+    k_thread_join(thread_b, K_FOREVER);
+    LOG_INF("counter: %u", counter);
     return 0;
 }
 
